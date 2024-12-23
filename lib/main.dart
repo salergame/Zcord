7b3e1c4ed6efd6173b256b215250c18529baa788
Sxlken <diasshag@gmail.com>
2024-12-23 15:33:25 +0500

    start

--- lib/main.dart
+++ lib/main.dart
@@ -220,4 +220,3 @@ class ZCordLoginPage extends StatelessWidget {
     );
   }
 }
-
--- lib/reg.dart
+++ lib/reg.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({super.key});
